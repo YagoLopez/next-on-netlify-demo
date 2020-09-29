aa616f4c58cefa829298c863fc9e70663dcf31c1
Yago Lopez <yago.lopez@gmail.com>
2020-09-29 14:28:49 +0200

    test 16

--- src/lambda/hostname.js
+++ src/lambda/hostname.js
@@ -1,5 +1,7 @@
 exports.handler = function(event, context, callback) {
-  console.log(`{\ncontext: ${JSON.stringify(context,null,2)},\nevent: ${JSON.stringify(event,null,2)}\n}`)
+  // console.log(`{\ncontext: ${JSON.stringify(context,null,2)},\nevent: ${JSON.stringify(event,null,2)}\n}`)
+  console.log('context', JSON.stringify(context,null,2))
+  console.log('event', JSON.stringify(event,null,2))
   callback(null, {
     statusCode: 200,
     body: `Your IP address ${event.headers.host}`
